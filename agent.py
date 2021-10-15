89c3960c1a984307b25657ac08f70a0df3ddfb5a
TaliahHorner <taliah.horner@postcodelottery.co.uk>
2021-10-15 10:47:14 +0100

    Replace agent.py from notebook

--- agent.py
+++ agent.py
@@ -1,24 +1,20 @@
-# we add this above so you can write this main agent code into a file for submission later
+# we add this above so you can write
+# this main agent code into a file for submission later
 
 # for kaggle-environments
 from lux.game import Game
-from lux.game_map import Cell, RESOURCE_TYPES
+# from lux.game_map import Cell
 from lux.constants import Constants
-from lux.game_constants import GAME_CONSTANTS
-from lux import annotate
-from agent_utils import *
-import copy
-import math
-import random
+from agent_utils import nearest_resource, find_resources, can_move
 
-# we declare this global game_state object so that state persists across turns so we do not need to reinitialize it all the time
+# we declare this global game_state object so that state persists across turns
+# so we do not need to reinitialize it all the time
 game_state = None
 
-# this is the basic agent definition. At the moment this agent does nothing (and actually will survive for a bit before getting consumed by darkness)
+
 def agent(observation, configuration):
     global game_state
 
-    ### Do not edit ###
     if observation["step"] == 0:
         game_state = Game()
         game_state._initialize(observation["updates"])
@@ -26,22 +22,36 @@ def agent(observation, configuration):
         game_state.id = observation.player
     else:
         game_state._update(observation["updates"])
-    
+
     actions = []
 
-    ### AI Code goes down here! ### 
+    # AI Code
     player = game_state.players[observation.player]
-    opponent = game_state.players[(observation.player + 1) % 2]
+    # opponent = game_state.players[(observation.player + 1) % 2]
     width, height = game_state.map.width, game_state.map.height
-    D = Constants.DIRECTIONS
-    directions = [Constants.DIRECTIONS.NORTH, Constants.DIRECTIONS.EAST, Constants.DIRECTIONS.SOUTH, Constants.DIRECTIONS.WEST]
-    
+    # directions = [Constants.DIRECTIONS.NORTH, Constants.DIRECTIONS.EAST,
+    # Constants.DIRECTIONS.SOUTH, Constants.DIRECTIONS.WEST]
+
     resources = find_resources(game_state.map)
-    
+    print(resources)
+
     for unit in player.units:
-        nearest = nearest_resource(unit.pos, Constants.RESOURCE_TYPES.WOOD, resources)
-        direct = unit.pos.direction_to(nearest) #  directions[random.randint(0,3)]
-        if unit.can_act() and can_move(width, height, unit, direct):
-            actions.append(player.units[0].move(direct))
+        if unit.get_cargo_space_left() == 0:
+            city = list(player.cities.values())[0]
+            tile_pos = city.citytiles[0].pos
+            if tile_pos == unit.pos:
+                actions.append(unit.transfer(city.id,
+                                             Constants.RESOURCE_TYPES.WOOD, 100
+                                             ))
+            else:
+                direction_home = unit.pos.direction_to(tile_pos)
+                actions.append(player.units[0].move(direction_home))
+        else:
+            nearest = nearest_resource(unit.pos,
+                                       Constants.RESOURCE_TYPES.WOOD,
+                                       resources)
+            direct = unit.pos.direction_to(nearest)
+            if unit.can_act() and can_move(width, height, unit, direct):
+                actions.append(player.units[0].move(direct))
 
     return actions
--- agent_utils.py
+++ agent_utils.py
@@ -1,49 +1,55 @@
-from lux.game_objects import *
-from lux.constants import *
-from lux.game_map import *
+from lux.game_objects import Position, Unit
+from lux.constants import Constants
+from lux.game_map import GameMap
 
-def can_move(width, height, agent : Unit, direction) -> bool:
+
+def can_move(width, height, agent: Unit, direction) -> bool:
     """
     Whether or not the unit can move out of bounds of the map
     """
     # check n
     if direction == "s":
-        return agent.pos.y != height - 1    
-    
+        return agent.pos.y != height - 1
+
     # check e
     if direction == "e":
         return agent.pos.x != width - 1
-    
+
     # check s
     if direction == "n":
         return agent.pos.y != 0
-    
+
     # check w
     if direction == "w":
         return agent.pos.x != 0
-    
-    
-def find_resources(game_map : GameMap):
+
+
+def find_resources(game_map: GameMap):
     resources = {}
-    
+
     for x in range(game_map.height):
         for y in range(game_map.width):
             cell = game_map.get_cell(x, y)
             if cell.has_resource():
-                resources[cell.resource.type] = resources.setdefault(cell.resource.type, []) + [cell]
-            
+                resources[cell.resource.type] = resources \
+                        .setdefault(cell.resource.type, []) + [cell]
+
     return resources
 
-def nearest_resource(pos : Position, resource_type : Constants.RESOURCE_TYPES, resources) -> Position:
+
+def nearest_resource(pos: Position,
+                     resource_type: Constants.RESOURCE_TYPES,
+                     resources) -> Position:
+
     specific_resources = resources[resource_type]
-    
+
     nearest = specific_resources[0].pos
     for cell in specific_resources[1:]:
         if pos.distance_to(cell.pos) < pos.distance_to(nearest):
             nearest = cell.pos
-    
+
     return nearest
+
 
 def deliver_resources():
     pass
-                 
